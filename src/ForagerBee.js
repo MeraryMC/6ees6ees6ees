aabcad9663cf6f73e19da1ac06b2390e0893e4b3
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-01-16 13:00:32 -0800

    implemented ES6 classes

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Grub {
+  constructor (){
+    super();
+
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.canFly = true;
+    this.treasureChest = [];
+
+  }
+
+  forage (){
+    this.treasureChest.push('treasure');
+  }
 };
